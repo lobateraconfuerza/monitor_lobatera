433f2e3b2d737d2f1c2f3373c74e85905cfa6ec5
lobateraconfuerza <lobateraconfuerza@gmail.com>
2025-07-11 05:37:56 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 //supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://TU_PROYECTO.supabase.co';
-const supabaseKey = 'TU_API_KEY';
+const supabaseUrl = 'https://vziaqtyfjuqhwmfqxqrv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6aWFxdHlmanVxaHdtZnF4cXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTE0MTksImV4cCI6MjA2NzI4NzQxOX0.pDEN6Jc7jDOYh-hUGxiOVIVXOCAU--2fg9U_gwgzklg';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
